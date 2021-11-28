65e5515ed3d71177bad394f2593312e9110eee67
rogerliu37 <rliueml@gmail.com>
2021-11-28 12:59:31 -0500

    added form + validation for name, age, and cellphone number

--- register.php
+++ register.php
@@ -13,8 +13,8 @@
 
     <?php
     // define variables and set to empty values
-    $nameErr = $emailErr = $genderErr = $websiteErr = "";
-    $name = $email = $gender = $comment = $website = "";
+    $nameErr = $ageErr = $phoneErr = "";
+    $name = $age = $phone = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (empty($_POST["name"])) {
@@ -27,37 +27,28 @@
             }
         }
 
-        if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
-        } else {
-            $email = test_input($_POST["email"]);
-            // check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
+        if (empty($_POST["age"])) {
+            $ageErr = "Age is required";
+        } 
+        else {
+            $age = test_input($_POST["age"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[0-9]{1,10}$/", $age)) {
+                $ageErr = "Invalid Age";
             }
         }
 
-        if (empty($_POST["website"])) {
-            $website = "";
-        } else {
-            $website = test_input($_POST["website"]);
-            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-                $websiteErr = "Invalid URL";
+        if (empty($_POST["phone"])) {
+            $phoneErr = "Phone is required";
+        } 
+        else {
+            $phone = test_input($_POST["phone"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[0-9]{10,10}$/", $phone)) {
+                $phoneErr = "Invalid Phone";
             }
         }
 
-        if (empty($_POST["comment"])) {
-            $comment = "";
-        } else {
-            $comment = test_input($_POST["comment"]);
-        }
-
-        if (empty($_POST["gender"])) {
-            $genderErr = "Gender is required";
-        } else {
-            $gender = test_input($_POST["gender"]);
-        }
     }
 
     function test_input($data)
@@ -75,19 +66,13 @@
         Name: <input type="text" name="name" value="<?php echo $name; ?>">
         <span class="error">* <?php echo $nameErr; ?></span>
         <br><br>
-        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
-        <span class="error">* <?php echo $emailErr; ?></span>
+        Age: <input type="text" name="age" value="<?php echo $age; ?>">
+        <span class="error">* <?php echo $ageErr; ?></span>
         <br><br>
-        Website: <input type="text" name="website" value="<?php echo $website; ?>">
-        <span class="error"><?php echo $websiteErr; ?></span>
+        phone: <input type="text" name="phone" value="<?php echo $phone; ?>">
+        <span class="error"><?php echo $phoneErr; ?></span>
         <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment; ?></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "other") echo "checked"; ?> value="other">Other
-        <span class="error">* <?php echo $genderErr; ?></span>
+
         <br><br>
         <input type="submit" name="submit" value="Submit">
     </form>
@@ -96,13 +81,9 @@
     echo "<h2>Your Input:</h2>";
     echo $name;
     echo "<br>";
-    echo $email;
+    echo $age;
     echo "<br>";
-    echo $website;
-    echo "<br>";
-    echo $comment;
-    echo "<br>";
-    echo $gender;
+    echo $phone;
     ?>
 
 </body>
