b34a685fe7514f5aa220d5f8bc4f24249013f446
rogerliu37 <rliueml@gmail.com>
2021-11-28 19:50:33 -0500

    Update number.php

--- number.php
+++ number.php
@@ -3,7 +3,9 @@
 <body>
     <form name="form" method="post">
         <input type="submit" name="button1" value="1 shot" />
-        <input type="submit" name="button2" value="fully vaccinated" />
+        <input type="submit" name="button2" value="Moderna fully vaccinated" />
+        <input type="submit" name="button2" value="Pfizer fully vaccinated" />
+        <input type="submit" name="button2" value="J&J fully vaccinated" />
        
     </form>
 </body>
@@ -47,17 +49,63 @@ if (isset($_POST['button2'])) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "select count(NumberOfDoses) FROM customer where NumberOfDoses = 2";
+    $sql = "select count(NumberOfDoses) FROM customer where NumberOfDoses = 2 and Manufacturer='Moderna'";
     $result = mysqli_query($conn, $sql);
     while ($row = mysqli_fetch_array($result)) {
-        echo "Number of people with at least one shot: " . $row[0]  . "   ";
+        echo "Number of people with Moderna fully vaccinated: " . $row[0]  . "   ";
     }
     if (mysqli_num_rows($result) == 0) {
-        echo "Nobody get at least 1 shot.";
+        echo "Nobody get fully vaccinated through Moderna.";
     }
 } //if isset
 
+if (isset($_POST['button3'])) {
 
+    $servername = "localhost";
+    $username = "root";
+    $password = "Ruijie0307!";
+    $dbname = "vaccine";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "select count(NumberOfDoses) FROM customer where NumberOfDoses = 2 and Manufacturer='Pfizer'";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+        echo "Number of people with Pfizer fully vaccinated: " . $row[0]  . "   ";
+    }
+    if (mysqli_num_rows($result) == 0) {
+        echo "Nobody get fully vaccinated through Pfizer.";
+    }
+} //if isset
+
+if (isset($_POST['button4'])) {
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "Ruijie0307!";
+    $dbname = "vaccine";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "select count(NumberOfDoses) FROM customer where NumberOfDoses = 1 and Manufacturer='Johnson and Johnson'";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+        echo "Number of people with J&J fully vaccinated: " . $row[0]  . "   ";
+    }
+    if (mysqli_num_rows($result) == 0) {
+        echo "Nobody get fully vaccinated through J&J.";
+    }
+} //if isset
 ?>
 
 </html>
